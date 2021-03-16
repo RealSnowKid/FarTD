5eb3b4214be8de7c7fe1700ea7ebb935dad2a778
i403318 <realsnowkid@gmail.com>
2021-03-16 14:03:41 +0200

    Add crouch to player controller

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
+    //player speed
+    private float speed;
 
-    public float speed = 5;
+    //speed variables
+    [Tooltip("The walking speed is the normal speed. While the sprint speed is this x1.5 and the crouched speed is this /2")]
+    public float walkingSpeed = 5f;
+    private float crouchedSpeed;
+
     public float jumpStrength = 5f;
 
+    //player height
     private float height;
+
+    //crouch variables
+    public float crouchHeight = 1f;
+    private float originalHeight;
+
+    private bool isCrouched = false;
 
     public float mouseSensitivity = 1;
     public float mouseSmoothing = 2;
+
+    CapsuleCollider playerCapColl;
 
     Vector2 velocity;
 
@@ -23,13 +38,18 @@ public class PlayerControl : MonoBehaviour {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
+        playerCapColl = GetComponent<CapsuleCollider>();
+        originalHeight = GetComponent<CapsuleCollider>().height;
         height = GetComponent<Collider>().bounds.extents.y;
+        speed = walkingSpeed;
+        crouchedSpeed = walkingSpeed / 2;
     }
 
     // Update is called once per frame
     void Update() {
         Move();
         Look();
+        Crouch();
     }
 
     bool isGrounded() {
@@ -41,7 +61,7 @@ public class PlayerControl : MonoBehaviour {
         velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         transform.Translate(velocity.x, 0, velocity.y);
 
-        if (Input.GetButtonDown("Jump") && isGrounded()) {
+        if (Input.GetButtonDown("Jump") && isGrounded() && !isCrouched) {
             GetComponent<Rigidbody>().AddForce(Vector3.up * 100 * jumpStrength);
         }
     }
@@ -56,5 +76,21 @@ public class PlayerControl : MonoBehaviour {
         //rotate cam and controller
         playerCamera.transform.localRotation = Quaternion.AngleAxis(-currentMouseLook.y, Vector3.right);
         transform.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up);
+    }
+
+    void Crouch()
+    {
+        if (Input.GetButtonDown("Crouch") && isGrounded())
+        {
+            playerCapColl.height = crouchHeight;
+            speed = crouchedSpeed;
+            isCrouched = true;
+        }
+        else if (Input.GetButtonUp("Crouch") && isGrounded())
+        {
+            playerCapColl.height = originalHeight;
+            speed = walkingSpeed;
+            isCrouched = false;
+        }
     }
 }
