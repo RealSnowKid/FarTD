4d3904edb59fb79447e88c55f224da99c1874069
i403318 <realsnowkid@gmail.com>
2021-03-17 16:47:36 +0200

    Add first version of player mining

---
+++ Assets/Scripts/NodeMining.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeMining : MonoBehaviour
+{
+    [Tooltip("The total amount of ore in the node")]
+    public float oreCapacity = 100f;
+    [Tooltip("The amount of ore mined every second")]
+    public float miningSpeed = 2f;
+    
+    private bool mining = false;
+
+    private void Update()
+    {
+        if (mining == true)
+        {
+            InvokeRepeating("MineOre", 1, 10);
+        }
+        if (oreCapacity <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.GetComponent("PlayerControl") != null)
+        {
+            if (Input.GetKey("e"))
+            {
+                mining = true;
+            }
+            else
+            {
+                mining = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.GetComponent("PlayerControl") != null)
+        {
+            mining = false;
+        }
+    }
+
+    void MineOre()
+    {
+            oreCapacity -= miningSpeed;
+            Debug.Log(oreCapacity);
+    }
+}
