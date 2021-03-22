8248b051806390f5d75d085de30d66d72e50c908
Vlado <i432274@fhict.nl>
2021-03-22 17:45:17 +0100

    conveyor offset fix

--- Assets/Scripts/ConveyorMovement.cs
+++ Assets/Scripts/ConveyorMovement.cs
@@ -10,12 +10,20 @@ public class ConveyorMovement : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
     }
 
+    /*
     private void OnCollisionStay(Collision collision) {
         if (collision.collider.gameObject.GetComponent<OreDrop>() != null) {
             Vector3 pos = rb.position;
 
-            rb.position += transform.forward * speed * Time.fixedDeltaTime * -1;
+            rb.position += -transform.forward * speed * Time.fixedDeltaTime;
             rb.MovePosition(pos);
+        }
+    }
+    */
+
+    private void OnCollisionStay(Collision collision) {
+        if (collision.collider.gameObject.GetComponent<OreDrop>() != null) {
+            collision.collider.gameObject.transform.Translate(transform.forward * speed * Time.fixedDeltaTime);
         }
     }
 }
