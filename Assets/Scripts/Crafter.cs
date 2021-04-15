62be36bad2fa830705c41146b1e33f78cd3fa6fe
i403318 <realsnowkid@gmail.com>
2021-04-15 11:20:31 +0300

    Changed some textures and made crafter logic

--- Assets/Scripts/Crafter.cs
+++ Assets/Scripts/Crafter.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Crafter : Building
 {
     private bool isBuilt = false;
+    [SerializeField] CraftingRecipe craftingRecipe;
+    private List<Item> items;
     
     public void Build()
     {
@@ -13,9 +15,35 @@ public class Crafter : Building
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.GetComponent<OreDrop>() != null)
+        if (collider.GetComponent<OreDrop>() != null && craftingRecipe == null && isBuilt == true)
         {
-            collider.gameObject.transform.Translate(new Vector3(0, 0, 0), Space.World);
+            Destroy(collider.gameObject);
+        }
+        else if(collider.GetComponent<OreDrop>() != null && craftingRecipe != null && isBuilt == true)
+        {
+            bool included = false;
+            if (collider.GetComponent<OreDrop>().Item != null)
+            {
+                Debug.Log("Item: " + collider.GetComponent<OreDrop>().Item);
+                foreach (ItemAmount ia in craftingRecipe.Materials)
+                {
+                    if (ia.Item == collider.GetComponent<OreDrop>().Item)
+                    {
+                        included = true;
+                        items.Add(collider.GetComponent<OreDrop>().Item);
+                        Debug.Log("Item added: " + collider.GetComponent<OreDrop>().Item);
+                        break;
+                    }
+                }
+                if (included == false)
+                {
+                    Destroy(collider.gameObject);
+                }
+            }
+            else
+            {
+                Destroy(collider.gameObject);
+            }
         }
     }
 }
