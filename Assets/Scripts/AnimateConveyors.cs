b1143474ccf1d81ab44aced7efc9d75a797d4fa8
Vlado <i432274@fhict.nl>
2021-04-21 03:30:52 +0200

    music and new models added

---
+++ Assets/Scripts/AnimateConveyors.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateConveyors : MonoBehaviour {
+    public Material conveyorMat;
+
+    private void Update() {
+        conveyorMat.mainTextureOffset -= new Vector2(0f, Time.deltaTime * 0.5f);
+    }
+}
