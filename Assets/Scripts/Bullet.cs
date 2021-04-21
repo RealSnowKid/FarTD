fee8ec7f208cd57d6d0b3a4186f7aad21e947983
Vlado <i432274@fhict.nl>
2021-04-21 19:05:33 +0200

    better bullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,4 +8,16 @@ public class Bullet : MonoBehaviour {
     public void SetDamage(float dmg) {
         damage = dmg;
     }
+
+    public void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.GetComponent<Enemy>() == null) {
+            GetComponent<ParticleSystem>().Play();
+
+            GetComponent<Rigidbody>().isKinematic = true;
+            Destroy(GetComponent<MeshRenderer>());
+            Destroy(GetComponent<Collider>());
+
+            Destroy(gameObject, 0.5f);
+        }
+    }
 }
